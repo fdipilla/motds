9d4458b17432b3b1a79b27aeb94f9b331feee3f6
Fabrizio Di Pilla <fabriziodipilla@gmail.com>
2014-04-11 13:03:41 -0300

    first commit

---
+++ motd.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+rojo='\e[1;31m'
+NC='\e[0m'
+verde='\e[1;32m'
+amarillo='\e[1;33m'
+azul='\e[1;34m'
+violeta='\e[1;35m'
+celeste='\e[1;36m'
+
+echo -e "" > /etc/motd.tail
+
+echo -e "${rojo}                                                _,met\$\$\$\$\$gg.       " >> /etc/motd.tail
+echo -e "                                             ,g\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$P.    " >> /etc/motd.tail
+echo -e "                                           ,g\$\$P\"\"       \"\"\"Y\$\$.\".  " >> /etc/motd.tail
+echo -e "                                          ,\$\$P'              \`\$\$\$.  " >> /etc/motd.tail
+echo -e "                                        ',\$\$P       ,ggs.     \`\$\$b: " >> /etc/motd.tail
+echo -e "                                        \`d\$\$'     ,\$P\"'   .    \$\$\$  " >> /etc/motd.tail
+echo -e "                                         \$\$P      d\$'     ,    \$\$P  " >> /etc/motd.tail
+echo -e "                                         \$\$:      \$\$.   -    ,d\$\$'  " >> /etc/motd.tail
+echo -e "                                         \$\$;      Y\$b._   _,d\$P'    " >> /etc/motd.tail
+echo -e "                                         Y\$\$.    \`.\`\"Y\$\$\$\$P\"'       " >> /etc/motd.tail
+echo -e "                                         \`\$\$b      \"-.__" >> /etc/motd.tail
+echo -e "                                          \`Y\$\$b                  " >> /etc/motd.tail
+echo -e "                                           \`Y\$\$.                 " >> /etc/motd.tail
+echo -e "                                             \`\$\$b.               " >> /etc/motd.tail
+echo -e "                                               \`Y\$\$b.            " >> /etc/motd.tail
+echo -e "                                                 \`\"Y\$b._         " >> /etc/motd.tail
+echo -e "                                                     \`\"\"\"\"       " >> /etc/motd.tail
+
+echo -e "${rojo} .----------------. ${verde} .----------------. ${amarillo} .----------------. ${azul} .----------------. ${violeta} .----------------. ${celeste} .-----------------." >> /etc/motd.tail
+echo -e "${rojo}| .--------------. |${verde}| .--------------. |${amarillo}| .--------------. |${azul}| .--------------. |${violeta}| .--------------. |${celeste}| .--------------. |" >> /etc/motd.tail
+echo -e "${rojo}| |  ________    | |${verde}| |  _________   | |${amarillo}| |   ______     | |${azul}| |     _____    | |${violeta}| |      __      | |${celeste}| | ____  _____  | |" >> /etc/motd.tail
+echo -e "${rojo}| | |_   ___ \`.  | |${verde}| | |_   ___  |  | |${amarillo}| |  |_   _ \    | |${azul}| |    |_   _|   | |${violeta}| |     /  \     | |${celeste}| ||_   \|_   _| | |" >> /etc/motd.tail
+echo -e "${rojo}| |   | |   \`. \ | |${verde}| |   | |_  \_|  | |${amarillo}| |    | |_) |   | |${azul}| |      | |     | |${violeta}| |    / /\ \    | |${celeste}| |  |   \ | |   | |" >> /etc/motd.tail
+echo -e "${rojo}| |   | |    | | | |${verde}| |   |  _|  _   | |${amarillo}| |    |  __'.   | |${azul}| |      | |     | |${violeta}| |   / ____ \   | |${celeste}| |  | |\ \| |   | |" >> /etc/motd.tail
+echo -e "${rojo}| |  _| |___.' / | |${verde}| |  _| |___/ |  | |${amarillo}| |   _| |__) |  | |${azul}| |     _| |_    | |${violeta}| | _/ /    \ \_ | |${celeste}| | _| |_\   |_  | |" >> /etc/motd.tail
+echo -e "${rojo}| | |________.'  | |${verde}| | |_________|  | |${amarillo}| |  |_______/   | |${azul}| |    |_____|   | |${violeta}| ||____|  |____|| |${celeste}| ||_____|\____| | |" >> /etc/motd.tail
+echo -e "${rojo}| |              | |${verde}| |              | |${amarillo}| |              | |${azul}| |              | |${violeta}| |              | |${celeste}| |              | |" >> /etc/motd.tail
+echo -e "${rojo}| '--------------' |${verde}| '--------------' |${amarillo}| '--------------' |${azul}| '--------------' |${violeta}| '--------------' |${celeste}| '--------------' |" >> /etc/motd.tail
+echo -e "${rojo} '----------------' ${verde} '----------------' ${amarillo} '----------------' ${azul} '----------------' ${violeta} '----------------'${celeste}  '----------------' " >> /etc/motd.tail
+echo -e "${NC}" >> /etc/motd.tail
+
