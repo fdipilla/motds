ce0a65065b2ab651b4c340408df96f836d0eb50b
fdipilla <fabriziodipilla@gmail.com>
2015-01-20 11:54:05 -0300

    Added batman motd

---
+++ batman/motd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+rojo='\e[1;31m'
+NC='\e[0m'
+verde='\e[1;32m'
+amarillo='\e[1;33m'
+azul='\e[1;34m'
+violeta='\e[1;35m'
+celeste='\e[1;36m'
+
+echo -e "" > /etc/motd
+
+
+echo -e "                   ${amarillo},.ood888888888888boo.," >> /etc/motd
+echo -e "               .od888P^\"\"            \"\"^Y888bo." >> /etc/motd
+echo -e "           .od8P''   ..oood88888888booo.    \`\`Y8bo." >> /etc/motd
+echo -e "        .odP'\"  .ood8888888888888888888888boo.  \"\`Ybo." >> /etc/motd
+echo -e "      .d8'   od8'd888888888f\`8888't888888888b\`8bo   \`Yb." >> /etc/motd
+echo -e "     d8'  od8^   8888888888[  \`'  ]8888888888   ^8bo  \`8b" >> /etc/motd
+echo -e "   .8P  d88'     8888888888P      Y8888888888     \`88b  Y8." >> /etc/motd
+echo -e "  d8' .d8'       \`Y88888888'      \`88888888P'       \`8b. \`8b" >> /etc/motd
+echo -e " .8P .88P            \"\"\"\"            \"\"\"\"            Y88. Y8." >> /etc/motd
+echo -e " 88  888                                              888  88" >> /etc/motd
+echo -e " 88  888                                              888  88" >> /etc/motd
+echo -e " 88  888.        ..                        ..        .888  88" >> /etc/motd
+echo -e " \`8b \`88b,     d8888b.od8bo.      .od8bo.d8888b     ,d88' d8'" >> /etc/motd
+echo -e "  Y8. \`Y88.    8888888888888b    d8888888888888    .88P' .8P" >> /etc/motd
+echo -e "   \`8b  Y88b.  \`88888888888888  88888888888888'  .d88P  d8'" >> /etc/motd
+echo -e "     Y8.  ^Y88bod8888888888888..8888888888888bod88P^  .8P" >> /etc/motd
+echo -e "      \`Y8.   ^Y888888888888888LS888888888888888P^   .8P'" >> /etc/motd
+echo -e "        \`^Yb.,  \`^^Y8888888888888888888888P^^'  ,.dP^'" >> /etc/motd
+echo -e "           \`^Y8b..   \`\`^^^Y88888888P^^^'    ..d8P^'" >> /etc/motd
+echo -e "               \`^Y888bo.,            ,.od888P^'" >> /etc/motd
+echo -e "                    \"\`^^Y888888888888P^^'\"         ${NC}" >> /etc/motd
+
