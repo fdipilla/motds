20b20f92fb591420634d1cdd9052a9ce0ac4c05b
fdipilla <fabriziodipilla@gmail.com>
2015-01-27 16:11:35 -0300

    Added gondor

---
+++ gondor/motd.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+echo -e "" > /etc/motd
+
+echo -e "                  .         .o:\`         .                  " >> /etc/motd
+echo -e "                 :h-                   \`/h.                 " >> /etc/motd
+echo -e "                  .     \`  .    \`  \`     .                  " >> /etc/motd
+echo -e "                        /  -:  /\`  /                        " >> /etc/motd
+echo -e "         .            \` o:- //s.\`::/ \`           \`\`         " >> /etc/motd
+echo -e "        -h:       \`  \`/ \`y-  Ns  ++  /   \`      \`ss\`        " >> /etc/motd
+echo -e "         .        \`:  s/:./o\`Ns.s.-:o/ \`/        \`\`         " >> /etc/motd
+echo -e "               -\`  :- o\` \` -dMmy\` \` /. +\`  -.               " >> /etc/motd
+echo -e "           \`-   o   s:/  :/-/Mm.//.  o-o   +  \`.            " >> /etc/motd
+echo -e "            \`/ /o:..:d\` ./ :sNhs. o  /h-\`::y\` +             " >> /etc/motd
+echo -e "    \`+-      o.+  ../\`//:.  +MN.  .//: o .  o/-     \`+-     " >> /etc/motd
+echo -e "    \`o-      \`yo  \`o/   \`/+o+Md/o+-\`  .o/  \`d+      \`o-     " >> /etc/motd
+echo -e "           \`. \`oo::/    \`\` \`dMMo \`\`\`    o./s:  -            " >> /etc/motd
+echo -e "            +:\`  ./hh+/:::+osMNoo/:::/oms:\`  .o.            " >> /etc/motd
+echo -e "          \` +/\`  \`+:   -/s//hMNs:/s:\`  \`+/   \`y\` \`          " >> /etc/motd
+echo -e "         \`s:-o::+/    o.::  /Mm\`  s :/   .++:/+./+          " >> /etc/motd
+echo -e "         -\` -/so     o+  +\` -Md  :- \`y:    \`h+:. :\`         " >> /etc/motd
+echo -e "              \`-  \`-/.\`\`    -Md     .\`::-  \`:               " >> /etc/motd
+echo -e "                            -Mm                             " >> /etc/motd
+echo -e "                            /MN                             " >> /etc/motd
+echo -e "                            /MN\`                            " >> /etc/motd
+echo -e "                            +MN\`                            " >> /etc/motd
+echo -e "                            +MM.                            " >> /etc/motd
+echo -e "                            oMM.                            " >> /etc/motd
+echo -e "                            sMM-                            " >> /etc/motd
+echo -e "                            yMM:                            " >> /etc/motd
+echo -e "                            hMM/                            " >> /etc/motd
+echo -e "                           -NMMd\`                           " >> /etc/motd
+echo -e "                         \`+NMMMMd:                          " >> /etc/motd
+echo -e "                  .:::oosmNhysyhdNhss+::-\`                  " >> /etc/motd
+
+
+echo -e "" >> /etc/motd
+
